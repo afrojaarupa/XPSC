ed8d36cc44de8101fb0edba03b9126d18af57caf
afrojaarupa <129607559+afrojaarupa@users.noreply.github.com>
2024-03-15 22:26:48 +0600

    Create A_Four_Digits.cpp

---
+++ Week 1/Day 1/A_Four_Digits.cpp
@@ -0,0 +1,21 @@
+/**   
+ * Problem: A - Four Digits
+ * Link: https://atcoder.jp/contests/abc222/tasks/abc222_a
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int sz = s.size();
+    if(sz == 4) cout << s << endl;
+    else 
+    {
+        if(sz == 1) cout << "000" << s << endl;
+        if(sz == 2) cout << "00" << s << endl;
+        if(sz == 3) cout << "0" << s << endl; 
+    }
+
+    return 0;
+}
