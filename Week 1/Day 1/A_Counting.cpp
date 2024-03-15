68392c9fafbd71e9c1e6c147d12e395439cfd2df
afrojaarupa <129607559+afrojaarupa@users.noreply.github.com>
2024-03-15 21:17:04 +0600

    Create A_Counting.cpp

---
+++ Week 1/Day 1/A_Counting.cpp
@@ -0,0 +1,17 @@
+/**   
+ * Problem: A - Counting
+ * Link: https://atcoder.jp/contests/abc209/tasks/abc209_a
+ */
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int ans = (b - a) + 1;
+    if(ans <= 1) cout << "0" << endl;
+    else cout << ans << endl;
+
+    return 0;
+}
