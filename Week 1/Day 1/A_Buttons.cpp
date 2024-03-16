454dcc99af5d9dd3228f9192c6f70e1c458677b7
afrojaarupa <129607559+afrojaarupa@users.noreply.github.com>
2024-03-16 20:48:03 +0600

    Update A_Buttons.cpp

--- Week 1/Day 1/A_Buttons.cpp
+++ Week 1/Day 1/A_Buttons.cpp
@@ -4,7 +4,6 @@
  */
 #include<bits/stdc++.h>
 using namespace std;
- 
 int main()
 {
     int a, b, ans1, ans2;
@@ -13,8 +12,7 @@ int main()
     ans1 = a + b;
     ans2 = a + a-1;
     
-    if(a > b) ans2 = a + a-1;
-    else ans2 = b + b-1;
+    if(b > a) ans2 = b + b-1;
         
     cout << max(ans1,ans2) <<endl;
 
