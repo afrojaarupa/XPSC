c35216ac3cf1713f4710686439fc2d415614774c
afrojaarupa <129607559+afrojaarupa@users.noreply.github.com>
2024-05-20 22:08:35 +0600

    Day 5

--- Week 8/Day 5/A_k_th_divisor.cpp
+++ Week 8/Day 5/A_k_th_divisor.cpp
@@ -3,21 +3,22 @@ using namespace std;
 #define ll long long int
 int main() 
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
     ll n, k;
     cin >> n >> k;
     vector<ll> div;
     for(ll i = 1; i*i <= n; i++)
     {
-        if((n % i) == 0) div.push_back(i);
-        if((n / i) != i) div.push_back(n/i);
+        if((n % i) == 0) 
+        {
+            div.push_back(i);
+            if((n / i) != i) div.push_back(n/i);
+        }
+        
     }
     sort(div.begin(), div.end());
 
-    if(k <= div.size()) cout << div[k - 1] << '\n';
-    else cout << "-1" << '\n';
+    if(k <= div.size()) cout << div[k - 1] << endl;
+    else cout << "-1" << endl;
 
     return 0;
 }
